7bc5be06db3cf1aaf02ba96d3c16f4cab9a8de89
Mgteixeira2112 <102971119+Mgteixeira2112@users.noreply.github.com>
2026-09-01 01:59:35 -0300

    refactor: remove legacy workspace renderer alias

--- src/workspace-engine/WidgetDrivenWorkspace.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { WorkspaceDefinition } from './types';
-import { VisualWorkspaceRuntime } from './VisualWorkspaceRuntime';
-
-/** @deprecated Use VisualWorkspaceRuntime. Kept temporarily as a source-compatible alias while stacked PRs settle. */
-export const WidgetDrivenWorkspace: React.FC<{ definition: WorkspaceDefinition }> = ({ definition }) =>
-  <VisualWorkspaceRuntime definition={definition} />;
